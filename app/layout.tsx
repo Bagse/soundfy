6cebd8b5825930935eec855da0a26fca8ab0bd0a
bagse <102260190+Bagse@users.noreply.github.com>
2023-06-17 22:04:18 -0500

    subscribe modal and account page

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ import ToasterProvider from "@/providers/ToasterProvider";
 import Head from "next/head";
 import getSongsByUserId from "@/actions/getSongsByUserId";
 import Player from "@/components/Player";
+import getActiveProductsWithPrices from "@/actions/getActiveProductsWithPrices";
 
 const font = Figtree({ subsets: ["latin"] });
 
@@ -24,6 +25,7 @@ export default async function RootLayout({
   children: React.ReactNode;
 }) {
   const userSongs = await getSongsByUserId();
+  const products = await getActiveProductsWithPrices();
 
   return (
     <html lang="en">
@@ -34,7 +36,7 @@ export default async function RootLayout({
         <ToasterProvider />
         <SupabaseProvider>
           <UserProvider>
-            <ModalProvider />
+            <ModalProvider products={products} />
             <Sidebar songs={userSongs}>{children}
             </Sidebar>
             <Player />
--- components/Library.tsx
+++ components/Library.tsx
@@ -2,21 +2,25 @@
 
 import { TbPlaylist } from "react-icons/tb";
 import { AiOutlinePlus } from "react-icons/ai";
-import useAuthModal from "@/hooks/useAuthModal";
-import { useUser } from "@/hooks/useUser";
-import useUploadModal from "@/hooks/useUploadModal";
+
 import { Song } from "@/types";
-import MediaItem from "./MediaItem";
+import useUploadModal from "@/hooks/useUploadModal";
+import { useUser } from "@/hooks/useUser";
+import useAuthModal from "@/hooks/useAuthModal";
+import useSubscribeModal from "@/hooks/useSubscribeModal";
 import useOnPlay from "@/hooks/useOnPlay";
+
+import MediaItem from "./MediaItem";
 
 interface LibraryProps {
   songs: Song[];
 }
 
 const Library: React.FC<LibraryProps> = ({ songs }) => {
-  const authModal = useAuthModal();
+  const { user, subscription } = useUser();
   const uploadModal = useUploadModal();
-  const { user } = useUser();
+  const authModal = useAuthModal();
+  const subscribeModal = useSubscribeModal();
 
   const onPlay = useOnPlay(songs);
 
@@ -25,9 +29,13 @@ const Library: React.FC<LibraryProps> = ({ songs }) => {
       return authModal.onOpen();
     }
 
-    // todo: check for suscripton
+    if (!subscription) {
+      return subscribeModal.onOpen();
+    }
+
     return uploadModal.onOpen();
   };
+
   return (
     <div className="flex flex-col">
       <div className="flex items-center justify-between px-5 pt-4">
@@ -38,14 +46,21 @@ const Library: React.FC<LibraryProps> = ({ songs }) => {
         <AiOutlinePlus
           onClick={onClick}
           size={20}
-          className="text-neutral-400 cursor-pointer hover:text-white transition"
+          className="
+            text-neutral-400 
+            cursor-pointer 
+            hover:text-white 
+            transition
+          "
         />
       </div>
       <div className="flex flex-col gap-y-2 mt-4 px-3">
         {songs.map((item) => (
-          <div>
-            <MediaItem onClick={(id: string) => onPlay(id)} key={item.id} data={item} />
-          </div>
+          <MediaItem
+            onClick={(id: string) => onPlay(id)}
+            key={item.id}
+            data={item}
+          />
         ))}
       </div>
     </div>
